a14f4c2586f0411d5f74a62fd574b9390f084fd1
MrKat303 <crisuaoriginal@gmail.com>
2026-09-02 06:41:25 -0400

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const poppins = localFont({
@@ -180,6 +181,7 @@ export default function RootLayout({
         }}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
